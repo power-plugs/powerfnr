83c53ee2fd8ff83ffc5d72852bae4e3da3422400
markthewolf90 <33386865+markthewolf90@users.noreply.github.com>
2020-01-22 17:09:23 -0700

    Fixed replacement changes not being applied
    Filtered out escape characters that result from double JSON stringifying the replacements to remedy changes being unapplyable. Also changed the layout of the code a bit and added an example of proper formatting until I can figure out how to define default values without them overwriting changes.

--- Settings.jsx
+++ Settings.jsx
@@ -9,8 +9,20 @@ module.exports = class Settings extends React.Component {
   }
 
   render () {
-    var dval = JSON.stringify(JSON.stringify(this.settings.get("replacements")));
+    var dval = JSON.stringify(JSON.stringify(this.settings.get("replacements"))).replace(/\\/g,'');
     dval = dval.substring(1, dval.length - 1);
-    return (<div><TextInput note='Current replacements (Stringified JSON).' defaultValue={dval} required={true} onChange={val => this.settings.set('replacements', JSON.parse(val), 1234)}>Code</TextInput></div>);
+    return (<div>
+	  <TextInput 
+	  note='Current replacements (Format as Stringified JSON). Example: 
+	  [[">:(","😡"],[":)","😄"],[":(","😦"]]'
+	  defaultValue={dval} 
+	  required={true} 
+	  onChange={(val) => {
+	    this.settings.set('replacements', JSON.parse(val), 1234)
+	  }}
+	  >
+	  Code
+	  </TextInput>
+	  </div>);
   }
 };

611def2602820ecd87a87b02df07cdca8f77fc42
Lil Sizzurp <lilsizzurp@protonmail.com>
2020-08-17 01:33:47 +0200

    Update deprecated functions (pt2)

--- Settings.jsx
+++ Settings.jsx
@@ -4,12 +4,10 @@ const { TextInput } = require('powercord/components/settings');
 module.exports = class Settings extends React.Component {
   constructor (props) {
     super();
-
-    this.settings = props.settings;
   }
 
   render () {
-    var dval = JSON.stringify(JSON.stringify(this.settings.get("replacements", []))).replace(/\\\"/g,'"');
+    var dval = JSON.stringify(JSON.stringify(this.getSetting("replacements", []))).replace(/\\\"/g,'"');
     dval = dval.substring(1, dval.length - 1);
     return (<div>
 	  <TextInput 
@@ -18,7 +16,7 @@ module.exports = class Settings extends React.Component {
 	  defaultValue={dval} 
 	  required={true} 
 	  onChange={(val) => {
-	    this.settings.set('replacements', JSON.parse(val), 1234)
+	    this.updateSetting('replacements', JSON.parse(val), 1234)
 	  }}
 	  >
 	  Code
